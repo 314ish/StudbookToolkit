76b5c3f2acba38ed43b58a83484c55a0f2a5e512
Goose <goose@honk.com>
2016-03-13 19:44:23 -0400

    filling out sparks tests

--- GTT/test/test_excel.py
+++ GTT/test/test_excel.py
@@ -6,14 +6,20 @@ import hashlib
 class TestExcelModule(unittest.TestCase):
 
     testFile = "testData/test_excel_data.xlsx"
+    recordsInTestFile = 11
+    testFileMD5 = "423171b7712f08069fe47300a36f8d1f"
 
     def test_excel_data_file_integrity(self):
         md5sum=hashlib.md5(open(self.testFile, 'rb').read()).hexdigest()
-        self.assertEqual(md5sum, "423171b7712f08069fe47300a36f8d1f")
+        self.assertEqual(md5sum, self.testFileMD5)
 
     def test_create_excel_writer(self):
         ew = excel.excelWriter("testWriter")
         self.assertIsInstance(ew, excel.excelWriter)
+
+    def test_write_excel_record(self):
+        ew = excel.excelWriter("testWriter")
+        # TODO: fill this test out
 
     def test_create_excel_reader(self):
         ew = excel.excelReader(self.testFile)
@@ -21,7 +27,7 @@ class TestExcelModule(unittest.TestCase):
 
     def test_read_all_lines(self):
         ew = excel.excelReader(self.testFile)
-        self.assertEqual(len(ew.getRecordsAsList()), 11)
+        self.assertEqual(len(ew.getRecordsAsList()), self.recordsInTestFile)
 
 
 if __name__ == '__main__':
--- GTT/test/test_sparks.py
+++ GTT/test/test_sparks.py
@@ -1,21 +1,25 @@
 import unittest
+import hashlib
+import sparks
 
 
-class TestStringMethods(unittest.TestCase):
+class TestSparksModule(unittest.TestCase):
 
-  def test_upper(self):
-      self.assertEqual('foo'.upper(), 'FOO')
+    testFile = "testData/test_sparks_data.dbf"
+    recordsInTestFile = 807
+    testFileMD5 = "3d7fb57bf61967786b55c745077d7cd4"
 
-  def test_isupper(self):
-      self.assertTrue('FOO'.isupper())
-      self.assertFalse('Foo'.isupper())
+    def test_sparks_data_file_integrity(self):
+        md5sum = hashlib.md5(open(self.testFile, 'rb').read()).hexdigest()
+        self.assertEqual(md5sum, self.testFileMD5)
 
-  def test_split(self):
-      s = 'hello world'
-      self.assertEqual(s.split(), ['hello', 'world'])
-      # check that s.split fails when the separator is not a string
-      with self.assertRaises(TypeError):
-          s.split(2)
+    def test_create_sparks_reader(self):
+        sp = sparks.SPARKSReader(self.testFile)
+        self.assertIsInstance(sp, sparks.SPARKSReader)
+
+    def test_read_all_lines(self):
+        sp = sparks.SPARKSReader(self.testFile)
+        self.assertEqual(len(sp.getRecordsAsList()), self.recordsInTestFile)
 
 if __name__ == '__main__':
     unittest.main()
